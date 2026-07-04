d2f177da811a13b7e15065ae46f6a6429695ab6a
vaseni2007 <sakthivelkalirajan79@gmail.com>
2026-07-04 12:42:57 +0530

    Add files via upload

---
+++ SmartwasteApplication.java
@@ -0,0 +1,13 @@
+package smartwaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartwasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartwasteApplication.class, args);
+	}
+
+}
